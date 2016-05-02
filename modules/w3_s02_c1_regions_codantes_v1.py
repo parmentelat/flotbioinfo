c04f6102f71318f8d9f484e4a2a0d6a5ef8a5db3
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2016-05-02 17:15:29 +0200

    revu l'algo de regions codantes (notebook, nbformatv2,   et le bout de python importé par la recherche multiphase/complém.-inv.

--- modules/w3_s02_c1_regions_codantes_v1.py
+++ modules/w3_s02_c1_regions_codantes_v1.py
@@ -17,6 +17,12 @@ from __future__ import print_function
 from __future__ import division
 
 
+# ### Recherche dans l'ADN ou dans l'ARN
+
+# Dans les vidéos, on a vu les valeurs des codons **Start** et **Stop** comme contenant des `U` - et pas de `T`. Remarquons en préambule qu'il est bien sûr équivalent de chercher `AUG` dans un ARN ou `ATG` dans l'ADN correspondant.
+# 
+# Aussi pour des raisons pratiques, puisque les brins de matériel génétique dont nous partons sont presque toujours de l'ADN, nous allons, dans tous les notebooks de cette semaine, chercher des codons contenant des `T` et non des `U`.
+
 # ### Recherche de marqueurs
 
 # Comme dans la vidéo, nous supposons que nous disposons de fonctions toutes faites pour la recherche de codons **Start** ou **Stop**. Pour l'instant nous les importons d'un autre module, mais rassurez-vous, nous verrons dès la prochaine séquence comment écrire nous-mêmes ces fonctions.
@@ -35,7 +41,7 @@ from w3_s03_c2_next_codon import next_start_codon, next_stop_codon
 
 # On précise par ailleurs que ces deux fonctions&nbsp;:
 #   * attendent en argument un brin `adn` et un `indice` de départ,
-#   * et retournent, soit l'indice de la prochaine occurrence à partir (y compris) de `indice` **et sur la même phase**, ou `None` s'il n'y a plus de correspondance au delà de `indice`
+#   * et retournent, soit l'indice de la prochaine occurrence à partir (y compris) de `indice` **et sur la même phase**, ou `None` s'il n'y a plus d'occurrence au delà de `indice`
 #  
 # Ainsi par exemple&nbsp;:
 
@@ -43,7 +49,7 @@ from w3_s03_c2_next_codon import next_start_codon, next_stop_codon
 
 # on trouve bien START car on part de l'indice 0 
 # et ATG se trouve à l'indice 6, donc sur la même phase
-next_start_codon("CGTACGATG", 0)
+###OFF next_start_codon("CGTACGATG", 0)
 
 
 # In[4]:
@@ -51,7 +57,7 @@ next_start_codon("CGTACGATG", 0)
 # par contre ici on ne trouve rien 
 # car l'indice de départ ne correspond pas à la phase 
 # sur laquelle se trouve ATG
-next_start_codon("CGTACGATG", 1)
+###OFF next_start_codon("CGTACGATG", 1)
 
 
 # ### L'instruction `break`
@@ -77,11 +83,11 @@ next_start_codon("CGTACGATG", 1)
 # Une fois tout ceci acquis, nous pouvons écrire une fonction `coding_regions_one_phase` qui travaille sur un brin d'ADN, et qui va suivre la logique décrite dans la vidéo. Notre fonction va prendre les arguments suivants&nbsp;:
 #   * le brin d'ADN
 #   * la phase, exprimée comme un entier 0, 1 ou 2
-#   * la taille minimale entre 2 deux **Stop**; ce dernier argument sera optionnel, lorsqu'il est omis on prendra comme valeur par défaut 300, comme dans le cours.
+#   * la taille minimale entre 2 deux **Stop**&nbsp;; ce dernier argument sera optionnel, lorsqu'il est omis on prendra comme valeur par défaut 300, comme dans le cours.
 #   
 # Ce qui nous donne le code suivant&nbsp;:
 
-# In[6]:
+# In[14]:
 
 # recherche de gènes selon l'heuristique décrite dans la vidéo
 # sur une phase seulement
@@ -93,44 +99,117 @@ def coding_regions_one_phase(adn, phase, minimal_length=300):
     # les résultats sont retournés sous la forme d'une liste 
     # de couples [start_gene, stop_gene]
     genes = []
+    # on calcule stop1 qui désigne pour nous le stop "de gauche"
+    # à ce stade, c'est le premier STOP à partir de la phase
+    stop1 = next_stop_codon(adn, index)
+    # s'il n'y a pas du tout de stop dans toute la séquence
+    # on a terminé
+    if not stop1:
+        return genes
     # la boucle 
     while True:      # boucle principale
-        # on cherche le premier STOP à partir de index
-        stop1 = next_stop_codon(adn, index)
+        # on cherche le premier STOP à partir de stop1
+        # pour trouver le stop "de droite"
+        stop2 = next_stop_codon(adn, stop1+3)
         # s'il n'y a plus de stop, on a fini
-        if not stop1:
+        if not stop2:
             return genes
-        # sinon on cherche un deuxieme stop
-        stop2 = stop1 + 3
         # mais il faut qu'il soit assez loin
-        while True:  # boucle interne
-            stop2 = next_stop_codon(adn, stop2)
-            # s'il n'y en a pas on a fini
-            if not stop2:
-                return genes
-            # s'il est assez loin, on peut sortir de la boucle interne
-            if stop2 - stop1 >= minimal_length:
-                break
-            # sinon il faut continuer à chercher un STOP
-            # pour passer au stop suivant et ne pas retomber a nouveau sur stop2
-            stop2 += 3
+        if stop2 - stop1 < minimal_length:
+            # c'est trop court, on saute ce fragment
+            stop1 = stop2
+            continue
         # à ce stade on a trouvé un ORF, reste à trouver le bon start
         start = next_start_codon(adn, stop1)
         # s'il n'y en a pas: c'est qu'on ne trouvera plus rien
-        # on a fini
+        # et donc on a fini
         if not start:
             return genes
         # ici c'est un peu subtil
         # tout d'abord rien ne nous dit qu'il y a un codon START entre start1 et start2
-        # et même s'il y en a un, rien de nous dit qu'on obtient un gêne assez long
-        # donc on reteste la longueur du gêne trouvé et on ne garde que ceux 
+        # et même s'il y en a un, rien de nous dit qu'on obtient un gène assez long
+        # donc on reteste la longueur du gène trouvé et on ne garde que ceux 
         # qui sont assez longs - par rapport à minimal_length
         if stop2 - start < minimal_length:
             pass
         else:
-            # cette fois on a trouvé un genes, on l'ajoute dans les résultats
+            # cette fois on a trouvé un gène, on l'ajoute dans les résultats
             genes.append( [start, stop2] )
         # on peut passer à l'ORF suivant
-        index = stop2 + 3
+        stop1 = stop2
 
 
+# ### Sur un exemple réel
+
+# Nous allons utiliser comme ADN source celui de [Bacillus subtilis](http://www.ebi.ac.uk/ena/data/view/CP010053) (clé `CP010053`), que pour des raisons de taille nous avons déjà importé&nbsp;:
+
+# In[15]:
+
+###OFF from samples import subtilis
+###OFF print("subtilis contient {} bases".format(len(subtilis)))
+
+
+# In[16]:
+
+# calculons les gènes sur la phase 0 avec cet algorithme
+###OFF genes = coding_regions_one_phase(subtilis, 0)
+###OFF print("On a trouvé {} gènes sur la phase 0".format(len(genes)))
+
+
+# ### Quelques statistiques (optionnel)
+
+# Pour ceux que cela pourrait intéresser, et qui ont quelques connaissances en python, voici quelques statistiques sur ce résultat. 
+
+# In[9]:
+
+# un tableau avec toutes les longueurs de gènes
+###OFF array_of_lengths = [ y-x for x,y in genes ]
+
+# la longueur totale de tous les gènes trouvés
+###OFF total_length = sum ( array_of_lengths )
+# la longueur moyenne des gènes
+###OFF average_length = total_length / len(genes)
+###OFF print('longueur moyenne des gènes', average_length)
+
+
+# In[10]:
+
+# les tailles minimale et maximale
+###OFF length_min = min ( array_of_lengths )
+###OFF length_max = max ( array_of_lengths )
+###OFF print("min = {}, max = {}".format(length_min, length_max))
+
+
+# In[11]:
+
+# pourcentage de la région codante par rapport à la longueur totale
+###OFF print("Pourcentage de région codante", total_length/len(subtilis))
+
+
+# ##### Un histogramme des longueurs
+
+# On peut simplement représenter la répartition des longueurs des gènes trouvés de la façon suivante. À nouveau ceci vous est donné surtout pour éveiller votre curiosité, n'hésitez pas à partager vos idées pour améliorer la présentation&nbsp;:
+
+# In[12]:
+
+# on importe matplotlib
+###OFF import matplotlib.pyplot as plt
+# pour que les figures apparaissent dans le notebook
+###OFF get_ipython().magic(u'matplotlib inline')
+
+# la taille utile pour les courbes
+###OFF import pylab
+###OFF pylab.rcParams['figure.figsize'] = 8., 8.
+
+
+# In[13]:
+
+# un histogramme de la répartition des longueurs 
+###OFF plt.hist(array_of_lengths, bins=75)
+###OFF plt.axis([300, 7700, 0, 400])
+###OFF plt.show()
+
+
+# ### Remarque de style
+
+# Signalons enfin pour les programmeurs puristes que de très nombreuses améliorations sont possibles, tant sur le style que sur les performances. On aurait pu par exemple se définir ici une classe `Gene` et retourner une liste de `Gene` plutot qu'une liste de listes&nbsp;; ou a minima utiliser des tuples plutôt que des listes. Je vous laisse ces améliorations à titre d'exercice, mais notre parti-pris pédagogique est de nous concentrer au maximum sur les algorithmes et d'utiliser python le plus simplement possible.
--- todo.md
+++ todo.md
@@ -10,19 +10,18 @@ un notebook d'exercice à intégrer `w3-e2-regexp-serine.ipynb`
 
 ***
 
-# TODO W3
+# DONE W3
 
 ## Séquence 2
 
+**DONE** L'algo de recherche de régions codantes est revisité pour tenir compte des remarques de François.
 
-Il me semble que l’algorithme de prédiction de gène est incorrect : une fois trouvé un stop, tu recherches le prochain stop en phase et tu testes la distance entre les deux. Si cette distance est trop faible, tu cherches un stop plus loin. Je pense que de ce fait, tu prédis des ORF qui contiennent un ou plusieurs stops, ce qui n’est pas correct. Pour éviter cela, il faudrait prendre ce 2e stop comme nouveau stop initial. Ton implémentation de l’algorithme peut avoir tendance à surprédire par la concaténation de très courtes ORFs.
+* avant: 15000 gènes sur les 6 phases de B. subtilis, c’est 4 fois trop
+* maintenant:  j'en trouve à présent 4723, ce qui semble déjà + correct
 
-15000 gènes sur les 6 phases de B. subtilis, c’est 4 fois trop.
+# TODO W3
 
-> oui en effet on considère des ORF avec possiblement un ou même des STOP dedans; mais si on ne fait pas ça comment on va pouvoir trouver des acides aminés correspondants au STOP ? En tous cas il y a clairement un truc que je n'ai pas compris..
-
-**TODO** ne plus considérer les STOP 
-**TODO** publier nouveau nombre de prédictions
+l'algorithme de recalage est une variante de la recherche de régions codantes; il faut le mettre à jour 
 
 # TODO W4
 
